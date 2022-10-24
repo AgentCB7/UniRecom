64ec8f39998139732481a25fecb3cdfcd59d85f4
Iamtithi <111843710+Iamtithi@users.noreply.github.com>
2022-10-24 14:49:10 +0700

    Add files via upload
    Admin file uploaded

---
+++ admin.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="design.css">
+
+</head>
+<body">
+    <div class="adminnav">
+        <img src="logo_black.png">
+        <nav>
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="survey.php">SURVEY</a></li>
+                <li><a href="#query">SEARCH</a></li>
+            </ul>
+        </nav>
+        <a href="logout_form.php"><button>Logout</button></a>
+    </div>
+    <div class="admincontent">
+        <h1>User Survey list</h1>
+        <br>
+        <table class="table">
+            <thead>
+    			<tr>
+    				<th>ID</th>
+    				<th>Name</th>
+    				<th>Gender</th>
+    				<th>Subject</th>
+    				<th>Class</th>
+                    <th>Finance</th>
+                    <th>Opinion</th>
+    			</tr>
+    		</thead>
+
+            <tbody>
+                <?php
+                $servername = "localhost";
+    			$username = "root";
+    			$password = "";
+    			$database = "test";
+
+    			// Create connection
+    			$connection = new mysqli($servername, $username, $password, $database);
+
+                // Check connection
+    			if ($connection->connect_error) {
+    				die("Connection failed: " . $connection->connect_error);
+    			}
+
+                // read all row from database table
+    			$sql = "SELECT * FROM survey";
+    			$result = $connection->query($sql);
+
+                if (!$result) {
+    				die("Invalid query: " . $connection->error);
+    			}
+
+                // read data of each row
+    			while($row = $result->fetch_assoc()) {
+                    echo "<tr>
+                        <td>" . $row["id"] . "</td>
+                        <td>" . $row["name"] . "</td>
+                        <td>" . $row["gender"] . "</td>
+                        <td>" . $row["subject"] . "</td>
+                        <td>" . $row["class"] . "</td>
+                        <td>" . $row["finance"] . "</td>
+                        <td>" .$row["opinion"] . "</td>
+                        <td>
+                            <a class='btn btn-primary btn-sm' href='edit.php?id=$row[id]'>Edit</a>
+                            <a class='btn btn-danger btn-sm' href='/CWH/delete.php?id=$row[id]'>Delete</a>
+                        </td>
+                    </tr>";
+                }
+
+                $connection->close();
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
